7d6e7cdfca2155b0942ae6340bc9447d850101a9
cxs7700 <christophersavan@gmail.com>
2022-08-07 02:26:13 -0400

    Fix bug of not setting button bg to correct light/dark mode color when mounted

--- components/Button/index.js
+++ components/Button/index.js
@@ -1,34 +1,38 @@
-import React from "react";
-import { useTheme } from "next-themes";
+import React, { useState, useEffect } from 'react';
+import { useTheme } from 'next-themes';
 
 const Button = ({ children, type, onClick, classes }) => {
-  const { theme } = useTheme();
-  if (type === "primary") {
-    return (
-      <button
-        onClick={onClick}
-        type="button"
-        className={`text-sm tablet:text-base p-1 laptop:p-2 m-1 laptop:m-2 rounded-lg ${
-          theme === "dark" ? "bg-white text-black" : "bg-black text-white"
-        }  transition-all duration-300 ease-out first:ml-0 hover:scale-105 active:scale-100`}
-      >
-        {children}
-      </button>
-    );
-  }
-  return (
-    <button
-      onClick={onClick}
-      type="button"
-      className={`text-sm tablet:text-base p-1 laptop:p-2 m-1 laptop:m-2 rounded-lg flex items-center transition-all ease-out duration-300 ${
-        theme === "dark"
-          ? "hover:bg-slate-600 text-white"
-          : "hover:bg-slate-100"
-      } hover:scale-105 active:scale-100  tablet:first:ml-0 ${classes}`}
-    >
-      {children}
-    </button>
-  );
+	const { theme } = useTheme();
+	const [mounted, setMounted] = useState();
+
+	useEffect(() => {
+		setMounted(true);
+	}, []);
+
+	if (type === 'primary') {
+		return (
+			<button
+				onClick={onClick}
+				type="button"
+				className={`text-sm tablet:text-base p-1 laptop:p-2 m-1 laptop:m-2 rounded-lg ${
+					mounted && theme === 'dark' ? 'bg-white text-black hover:bg-slate-600' : 'bg-black text-white'
+				}  transition-all duration-300 ease-out first:ml-0 hover:scale-105 active:scale-100`}
+			>
+				{children}
+			</button>
+		);
+	}
+	return (
+		<button
+			onClick={onClick}
+			type="button"
+			className={`text-sm tablet:text-base p-1 laptop:p-2 m-1 laptop:m-2 rounded-lg flex items-center transition-all ease-out duration-300 ${
+				mounted && theme === 'dark' ? 'hover:bg-slate-600 text-white' : 'hover:bg-slate-100'
+			} hover:scale-105 active:scale-100  tablet:first:ml-0 ${classes}`}
+		>
+			{children}
+		</button>
+	);
 };
 
 export default Button;
